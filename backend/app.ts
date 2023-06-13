9c4f1b9ca871d252f8b1e7c8d0052862f384e896
Folawewo Anthony <famwallexie@gmail.com>
2023-06-13 14:00:11 +0100

    .

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors"
 import * as dotenv from "dotenv";
 dotenv.config;
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 
 module.exports = app;
